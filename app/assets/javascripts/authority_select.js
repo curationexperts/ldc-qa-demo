046851ceb4ee297892f80268834a40c5630938fd
Jamie Little <jamie@jamielittle.org>
2017-02-27 18:50:33 -0500

    Updating JS

--- app/assets/javascripts/authority_select.js
+++ app/assets/javascripts/authority_select.js
@@ -1,16 +1,14 @@
 var authoritySelect = function(options) {
-    
-    
-    $(options.select).on('change', function(data) {
+    $(options.selectBox).on('change', function(data) {
 	var selection = $(this).val();
-	$(options.input).each(function(data) { $(this).data('autocomplete-url',selection) });
+	$(options.inputField).each(function(data) { $(this).data('autocomplete-url',selection) });
 	Hyrax.autocomplete();
     });
     
-    $('.work_subject').on('click', '.controls-add-text' ,function(data) {
+    $(options.inputClass).on('click', '.controls-add-text' ,function(data) {
 	var selection = $(options.select).val();
 	$(document).on('DOMNodeInserted', function() {
-	$(options.input).each(function(data) { $(this).data('autocomplete-url',selection) });
+	    $(options.input).each(function(data) { $(this).data('autocomplete-url',selection) });
 	    Hyrax.autocomplete();
 	});
     });  
