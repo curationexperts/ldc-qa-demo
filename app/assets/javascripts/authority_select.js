5e2629f1dedf3c1fcef80bb0f5faef3657e10788
Jamie Little <j.little@miami.edu>
2017-02-28 08:50:37 -0500

    add observer

--- app/assets/javascripts/authority_select.js
+++ app/assets/javascripts/authority_select.js
@@ -1,16 +1,18 @@
-var authoritySelect = function(options) {
-    $(options.selectBox).on('change', function(data) {
-	var selection = $(this).val();
-	$(options.inputField).each(function(data) { $(this).data('autocomplete-url',selection) });
-	Hyrax.autocomplete();
-    });
-    
-    $(options.inputClass).on('click', '.controls-add-text' ,function(data) {
-	var selection = $(options.selectBox).val();
-	$(document).on('DOMNodeInserted', function() {
-	    $(options.inputField).each(function(data) { $(this).data('autocomplete-url',selection) });
-	    Hyrax.autocomplete();
+var authoritySelect = function (options) {
+	$(options.selectBox).on('change', function (data) {
+		var selection = $(this).val();
+		$(options.inputField).each(function (data) { $(this).data('autocomplete-url', selection) });
+		Hyrax.autocomplete();
 	});
-    });  
+	
+	var observer = new MutationObserver(function (mutations) {
+		mutations.forEach(function (mutation) {
+		    $(options.inputField).each(function (data) { $(this).data('autocomplete-url', $(options.selectBox).val()) });
+			Hyrax.autocomplete();
+		});
+	});
+
+	var config = { childList: true };
+	observer.observe(document.body, config);
 }
 
