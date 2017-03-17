d00d6cba8539711efe1b0dea073483c61850aa8f
Jamie Little <jamie@jamielittle.org>
2017-03-17 16:26:23 -0400

    Change JS to use code PR'd into Hyrax

--- app/assets/javascripts/creator.js
+++ app/assets/javascripts/creator.js
@@ -1,5 +1,3 @@
 Blacklight.onLoad( function() {
-	var authoritySelect = require('authority_select');
-	var as = new authoritySelect.AuthoritySelect({ selectBox : "select#work_creator", inputField : "input.work_creator" });
-	as.initialize();
+	Hyrax.authoritySelect({ selectBox : "select#work_creator", inputField : "input.work_creator" });
 });
